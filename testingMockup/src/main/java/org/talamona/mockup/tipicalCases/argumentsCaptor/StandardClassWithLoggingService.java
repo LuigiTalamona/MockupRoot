3968bba4edae18ec70b5b77a5f0f49f1eb21362b
Luigi Talamona <luigi@talamona.org>
2013-08-20 16:26:56 +0200

    adding new test

--- testingMockup/src/main/java/org/talamona/mockup/tipicalCases/argumentsCaptor/StandardClassWithLoggingService.java
+++ testingMockup/src/main/java/org/talamona/mockup/tipicalCases/argumentsCaptor/StandardClassWithLoggingService.java
@@ -3,7 +3,6 @@ package org.talamona.mockup.tipicalCases.argumentsCaptor;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 
 /**
@@ -20,20 +19,32 @@ public class StandardClassWithLoggingService {
 
     }
     public boolean doSomethingWithNoparamsWritingLogs(){
-        Calendar date = Calendar.getInstance(Locale.ITALY);
-        SimpleDateFormat df = (SimpleDateFormat) DateFormat.getDateInstance(DateFormat.FULL, Locale.ITALY);
-
-
-        df.applyPattern("dd MMMMM yyyy hh:mm:ss");
-        String dateFormatedd = df.format(date.getTime());
-
-
-
-
+        String dateFormatedd = getFormattedDate();
         logger.info("info log wrote on " + dateFormatedd + " by " + this.getClass().getCanonicalName());
         logger.warn("warning log wrote on " + dateFormatedd + " by " + this.getClass().getCanonicalName());
         logger.error("error log wrote on " + dateFormatedd + " by " + this.getClass().getCanonicalName());
         logger.debug("debug log wrote on " + dateFormatedd + " by " + this.getClass().getCanonicalName());
         return true;
+    }
+
+
+    public boolean doSomethingWithParamsWritingLogs(String string){
+        String dateFormatedd = getFormattedDate();
+        logger.info("info log wrote on " + dateFormatedd + this.prepareLogMessage(string));
+        logger.warn("info log wrote on " + dateFormatedd + this.prepareLogMessage(string));
+        logger.error("info log wrote on " + dateFormatedd + this.prepareLogMessage(string));
+        logger.debug("info log wrote on " + dateFormatedd + this.prepareLogMessage(string));
+        return true;
+        
+        
+    }
+    private String prepareLogMessage(String string){
+        return " by method doSomethingWithParamsWritingLogs(" + string + ") of " +  this.getClass().getCanonicalName();
+    }
+    private String getFormattedDate() {
+        Calendar date = Calendar.getInstance(Locale.ITALY);
+        SimpleDateFormat df = (SimpleDateFormat) DateFormat.getDateInstance(DateFormat.FULL, Locale.ITALY);
+        df.applyPattern("dd MMMMM yyyy hh:mm:ss");
+        return df.format(date.getTime());
     }
 }
